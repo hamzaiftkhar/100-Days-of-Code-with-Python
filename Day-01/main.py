cc52057733f11bd7c29666c6111a278199742f81
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-09 23:32:19 +0500

    Introduction

--- Day-01/main.py
+++ Day-01/main.py
@@ -1,8 +1,9 @@
 #Hello Guys
 # Welcome to 100 days of code in python with Hamza Iftikhar
-# You have to stick with me for 100 days and here i will tell you the all basics of python and gradually we will move to the advance level
-# so be consistent with me and you will surely have good concepts of python at the end of this course
-# so lets start with the basics of python
+# You have to stick with me for 100 days and here i will tell you the all basics of python
+#And gradually we will move to the advance level
+#So be consistent with me and you will surely have good concepts of python at the end of this course
+#lets start with the basics of python
 
 # Day 1
 # Topic: Introduction to python and its applications
