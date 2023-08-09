29cbea3385091875e26dd436978bab2847575291
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-09 23:30:09 +0500

    Introduction file

---
+++ Day-01/main.py
@@ -0,0 +1,14 @@
+#Hello Guys
+# Welcome to 100 days of code in python with Hamza Iftikhar
+# You have to stick with me for 100 days and here i will tell you the all basics of python and gradually we will move to the advance level
+# so be consistent with me and you will surely have good concepts of python at the end of this course
+# so lets start with the basics of python
+
+# Day 1
+# Topic: Introduction to python and its applications
+# For detailed view must review readme file
+
+print("Welcome to 100 days of code in python with Hamza Iftikhar")
+print("Hello World")
+
+# Be motivated, consistent and focused
