f452872bb53e857bbfc68483b7db5b3dc9f9daa2
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-10 22:58:58 +0500

    Main file updated

--- Day-02/main.py
+++ Day-02/main.py
@@ -30,3 +30,33 @@ print(pi)
 
 #------------------------------------------------------------
 
+# we can install multiple pip packages for different projects according to their requirements
+
+# pip install <package-name>   (basic syntax to install a package)
+
+# pip install <package-name>==<version-number>   (to install a specific version of a package)
+
+# pip install <package-name> --upgrade   (to upgrade a package to its latest version)
+
+# pip uninstall <package-name>   (to uninstall a package)
+
+# pip list   (to list all the packages installed in the current environment)
+
+# pip freeze   (to list all the packages installed in the current environment along with their version)
+
+# pip freeze > requirements.txt   (to save all the packages installed in the current environment in a file named requirements.txt)
+
+# Examples of some packages
+
+# pip install numpy
+# pip install pandas
+# pip install matplotlib    (to plot graphs)
+# pip install pandas
+# pip install requests
+# pip install tensorflow
+# pip install os
+# pip install cv2
+# pip install turtle
+
+# along with these packages, we can also install some IDEs like PyCharm, Spyder, Jupyter Notebook, etc.
+# we use these packages to make our work easier and faster
