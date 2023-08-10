2f0b9a8d21f2c6fde2d553c6cf44b56603e59a7a
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-10 22:44:39 +0500

    Code files including example of content described in Day-02 README file

---
+++ Day-02/main.py
@@ -0,0 +1,32 @@
+# import standard math module 
+import math
+
+# use math.pi to get value of pi
+print("The value of pi is", math.pi)
+# The value of pi is 3.141592653589793
+
+# use math.sqrt to get square root of a number
+print("The square root of 16 is", math.sqrt(16))
+# The square root of 16 is 4.0
+
+
+#------------------------------------------------------------
+
+# import module by renaming it
+import math as m
+
+print(m.pi)
+
+# Output: 3.141592653589793
+
+#------------------------------------------------------------
+
+# import only pi from math module
+from math import pi
+
+print(pi)
+
+# Output: 3.141592653589793
+
+#------------------------------------------------------------
+
