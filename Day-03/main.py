dc381739935fd0f815de575d2849533160e58743
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-11 22:20:12 +0500

    main code file of Day 3

---
+++ Day-03/main.py
@@ -0,0 +1,62 @@
+# Our First Python Program with single line comment
+
+print("Hello World!")
+
+# Output: Hello World!
+
+print(4 + 5)
+# Output: 9
+
+print(9 * 7)
+# Output: 63
+
+print()
+# Output: (blank line)
+#----------------------------------------------------------
+
+# Example-2: Multi-line comment
+#Like C, Python supports multi-line comments as well.
+# All characters between the "#" character and the end of the physical line are part of the comment, and the Python interpreter ignores them. In this way we can also add multiline comments in Python.
+
+"""" This is a multiline
+comment. I can write as many lines as I want
+and it will not affect the code """
+print("Here above we can see a multiline comment.")
+
+# Output: Here above we can see a multiline comment.
+
+#----------------------------------------------------------
+
+# Examples of Escape Sequence characters in Python
+
+# using triple quotes
+print('''She said, "What's there?"''')
+# Output: She said, "What's there?"
+
+# escaping single quotes
+print('She said, "What\'s there?"')
+# Output: She said, "What's there?"
+
+# escaping double quotes
+print("She said, \"What's there?\"")
+# Output: She said, "What's there?"
+
+# using newline character
+print("Hello\nWe are learning Python")
+# Output:
+# Hello
+# We are learning Python
+
+# using tab character
+print("Hello\t Welcome to Day 3")
+# Output: Hello   Welcome to Day 3
+
+# using backslash
+print("This will insert \\ (backslash)")
+# Output: This will insert \ (backslash)
+
+# printing path
+print("C:\\Python\\Day-03")
+# Output: C:\Python\Day-03
+
+#----------------------------------------------------------
