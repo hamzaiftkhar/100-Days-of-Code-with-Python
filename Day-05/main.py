4f048088ea0e2e1b6364bb65b461c70046d4d875
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-13 23:32:20 +0500

    Main examples uploaded in main.py

--- Day-05/main.py
+++ Day-05/main.py
@@ -48,3 +48,69 @@ print(quadratic)
 #5
 
 #----------------------------------------------
+
+
+# Python code to demonstrate working of iskeyword()
+ 
+# importing "keyword" for keyword operations
+import keyword
+ 
+# printing all keywords at once using "kwlist()"
+print("The list of keywords is : ")
+print(keyword.kwlist)
+
+#----------------------------------------------
+
+print(False == 0)
+print(True == 1)
+
+print(True + True + True)
+print(True + False + False)
+
+print(None == 0)
+print(None == [])
+
+print(True or False)
+ 
+# showing logical operation
+# and (returns False)
+print(False and True)
+ 
+# showing logical operation
+# not (returns False)
+print(not True)
+
+#Output:    
+#True
+#True
+#3
+#1
+#False
+#False
+#True
+#False
+#False
+
+#----------------------------------------------
+
+my_variable1 = 20
+my_variable2 = "GeeksForGeeks"
+
+# check if my_variable1 and my_variable2 exists
+print(my_variable1)
+print(my_variable2)
+
+# delete both the variables
+del my_variable1
+del my_variable2
+
+# check if my_variable1 and my_variable2 exists
+print(my_variable1)
+print(my_variable2)
+
+#Output:
+#20
+#GeeksForGeeks
+#NameError: name 'my_variable1' is not defined
+
+#----------------------------------------------
