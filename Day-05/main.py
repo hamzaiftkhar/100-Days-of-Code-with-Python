3cee5cc1246e9ab8a790c9821cfd1d29996166f7
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-13 23:20:06 +0500

    examples of operators added in main.py file

---
+++ Day-05/main.py
@@ -0,0 +1,50 @@
+# examples of operators in python
+
+#Input:
+x = 5
+y = 3
+sum = x + y
+sub = x - y
+mul = x * y
+div = x / y
+mod = x % y
+exp = x ** y
+flo = x // y
+
+print ("Sum of x and y is: ", sum)
+print ("Subtraction of x and y is: ", sub)
+print ("Multiplication of x and y is: ", mul)
+print ("Division of x and y is: ", div)
+print ("Modulus of x and y is: ", mod)
+print ("Exponent of x and y is: ", exp)
+print ("Floor division of x and y is: ", flo)
+
+#Output:
+#Sum of x and y is:  8
+#Subtraction of x and y is:  2
+#Multiplication of x and y is:  15
+#Division of x and y is:  1.6666666666666667
+#Modulus of x and y is:  2
+#Exponent of x and y is:  125
+#Floor division of x and y is:  1
+
+#----------------------------------------------
+
+ratio = (1+(5**(1/2)))/2
+print(ratio)
+
+#Output:
+#1.618033988749895
+
+print (((5 + 4) / 3) * 2)
+
+#Output:
+#6
+
+quadratic = (3 * (2 ** 2)) - (4 * 2) + 1
+print(quadratic)
+
+#Output:
+#5
+
+#----------------------------------------------
