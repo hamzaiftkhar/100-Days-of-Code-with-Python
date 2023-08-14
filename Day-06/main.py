482576e5e56f6895b866767e37ea7cbe3ac32e7a
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-14 23:42:00 +0500

    typecasting examples

---
+++ Day-06/main.py
@@ -0,0 +1,56 @@
+# Typecasting Examples
+
+# Python program to demonstrate
+# implicit type Casting
+
+# Python automatically converts
+# a to int
+a = 7
+print(type(a))
+
+# Python automatically converts
+# b to float
+b = 3.0
+print(type(b))
+
+# Python automatically converts
+# c to float as it is a float addition
+c = a + b
+print(c)
+print(type(c))
+
+# Python automatically converts
+# d to float as it is a float multiplication
+d = a * b
+print(d)
+print(type(d))
+
+# Python automatically converts
+# e to float as it is a float division
+e = b / a
+print(e)
+print(type(e))
+
+# Python program to demonstrate
+# type Casting
+
+# int variable
+a = 5
+
+# typecast to str
+n = str(a)
+
+print(n)
+print(type(n))
+
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+
+print(c + d)
