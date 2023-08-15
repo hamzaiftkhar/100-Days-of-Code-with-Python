d0ce07bcac51741fc928214f020384056363f214
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-15 21:06:14 +0500

    Examples code added

---
+++ Day-07/main.py
@@ -0,0 +1,33 @@
+# Useful examples for exercise Day 7
+
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
+
+#-----------------#
+
+velocity = 10   #meter per second
+time = 5    # seconds
+distance = velocity * time
+acceleration = velocity / time
+print(distance)
+print(acceleration)
+
+#-----------------#
+
+print(5 == 5)
+# True
+print(5 == 6)
+# False
+print(5 != 6)
+# True
+print(5 > 6)
+# False
+print(5 < 6)
+# True
+
+#-----------------#
