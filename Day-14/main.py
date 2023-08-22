02d57cc2004dc5d09add356500ccf1bf09b65ea5
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-22 21:41:16 +0500

    Hint added

---
+++ Day-14/main.py
@@ -0,0 +1,13 @@
+# Hint for Date and time module:
+
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+
+#-----------------#
