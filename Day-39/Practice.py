ebd15dc5f3eea345f1ee7d7ac216a481cc670a22
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-09-21 23:20:25 +0500

    Practice file made to import in main file

---
+++ Day-39/Practice.py
@@ -0,0 +1,30 @@
+# This file will be imported to Day-39/main.py
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return  a * b
+
+def div(a, b):
+    return a / b
+
+def mod(a, b):
+    return a % b
+
+def power(a, b):
+    return a ** b
+
+def floor(a, b):
+    return a // b
+
+def main():
+    print("This is a practice file.")
+
+if __name__ == "__main__":
+    main()
+
+# End of file
