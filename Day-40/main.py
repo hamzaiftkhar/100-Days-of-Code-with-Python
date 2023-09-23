7fc0d46ecc3b8f83d726bfb82e7b267db68bebd9
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-09-23 23:14:50 +0500

    Main file added

---
+++ Day-40/main.py
@@ -0,0 +1,18 @@
+# This is main file here we will import the Practice.py file and will call the hello() and bye() function
+
+import Practice
+
+print(__name__)
+
+Practice.hello()
+Practice.bye()
+
+print("This is the main program")
+
+# Now if we run this program then it will print the __name__ as Practice and will call the hello() and bye() function
+
+# Practice
+# __main__
+# I am the Hello function
+# I am the Bye function
+# This is the main program
