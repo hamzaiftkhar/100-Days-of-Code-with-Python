6d98f59ea61abec4ec89249ff18cdbbde118ae3a
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-09-28 20:05:20 +0500

    Solution added

---
+++ Day-43/Question-3.py
@@ -0,0 +1,33 @@
+"""
+A text file named "matter.txt" contains some text, which needs to be displayed such that every next character is separated by a symbol "#". Write a function definition for hash_display() in Python that would display the entire content of the file matter.txt in the desired format.
+
+```
+Test Case:
+If the file "matter.txt" contains the following lines:
+
+Hello World
+
+Output:
+H#e#l#l#o# #W#o#r#l#d
+```
+"""
+
+# Code:
+
+#Create a file matter.txt and write the following lines in it
+
+# text = ["Hello World"]
+
+# file = open("matter.txt","w")
+# for line in text:
+#     file.write(line+"\n")
+# file.close()
+
+# def hash_display():
+#     file = open("matter.txt","r")
+#     data = file.read()
+#     for char in data:
+#         print(char,end="#")
+#     file.close()
+
+# hash_display()
