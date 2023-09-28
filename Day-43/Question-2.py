3febaf5b1066453e8098c2b245e45c8eb1f5f9ed
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-09-28 20:03:21 +0500

    Question 2 solution added

---
+++ Day-43/Question-2.py
@@ -0,0 +1,40 @@
+"""
+### Question-2
+
+Write a function in Python to count and display the total number of words in a text file ?
+
+```
+Test Case:
+If the file contain following items :
+
+Hello world
+This is Python Programming
+Language
+
+Output:
+7
+```
+"""
+
+# Code:
+
+#Create a file story.txt and write the following lines in it
+
+# text = ["Hello world","This is Python Programming","Language"]
+
+# file = open("story.txt","w")
+# for line in text:
+#     file.write(line+"\n")
+# file.close()
+
+# def count_words():
+#     file = open("story.txt","r")
+#     count = 0
+#     data = file.read()
+#     words = data.split()
+#     for word in words:
+#         count += 1
+#     print("Total words are",count)
+#     file.close()
+
+# count_words()
